3a9570eaff784b17ce13fc3af5208fa18f762b70
FredericoTimbo <fredericotimbo@gmail.com>
2025-11-24 01:14:14 -0300

    Primeiro Commit

---
+++ ApiAgendaTelefonica/Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using ApiAgendaTelefonica.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiAgendaTelefonica.Controllers
+{
+    public class TodoController : ControllerBase
+    {
+        List<Todo> todos = new List<Todo>
+
+        {
+            new Todo {Id = 1, Title = "Comprar leite", IsCompleted = true},
+            new Todo {Id = 2, Title = "Estudar AspNet", IsCompleted = true},
+            new Todo {Id = 3, Title = "Fazer exercícios", IsCompleted = false},
+        };
+
+        [Route("/api/todo")]
+        public IActionResult GetAllTodos()  
+        {
+            return Ok();
+
+        }
+    }
+}
---
+++ ApiAgendaTelefonica/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiAgendaTelefonica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder();
+            builder.Services.AddControllers();
+            var app = builder.Build();
+            app.MapControllers();
+            app.Run();
+
+        }
+    }
+}
